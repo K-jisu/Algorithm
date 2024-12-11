c7fd6601bdf9db13b812c195da5bcc0c0e394535
KJSoo95 <90014581+KJSoo95@users.noreply.github.com>
2024-12-12 03:11:20 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 2.62 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.js
@@ -1,7 +1,6 @@
 function solution(s) {
     var answer = true;
     var arr = [...s]
-    var regex = /\d/g;
     if(s.length === 4|| s.length ===6){
         console.log("4or6")
         for(i=0;i<arr.length;i++){
