55fde6994589acd223229182ffa9d45068afda4b
Jisu Kang <90014581+K-jisu@users.noreply.github.com>
2025-01-07 23:41:48 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,31 +1,13 @@
 function solution(s) {
-    var answer = "";
-    var word = ""
-    // 매핑 테이블
-    var table = {
-        zero : "0",
-        one : "1",
-        two : "2",
-        three : "3",
-        four : "4",
-        five : "5",
-        six : "6",
-        seven : "7",
-        eight : "8",
-        nine : "9",
+    let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = s;
+
+    for(let i=0; i< numbers.length; i++) {
+        let arr = answer.split(numbers[i]);
+        answer = arr.join(i);
     }
-    // 문자열 돌면서 매핑테이블과 일치하는지 비교
-    for(char of s){
-        if(isNaN(char)){
-            // console.log(char)
-            word += char;
-            if(table[word]){
-                answer += table[word]
-                word = "";
-            }
-        }else{
-            answer += char;
-        }
-    }
-    return +answer;
+
+    return Number(answer);
 }
+
+// 다른사람 풀이 (깔끔한듯)
