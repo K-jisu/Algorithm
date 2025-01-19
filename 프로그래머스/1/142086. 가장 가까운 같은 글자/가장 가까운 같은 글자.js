9109f2ce01fd85f0cc20a5e494a5c59a7348ab4f
Jisu Kang <90014581+K-jisu@users.noreply.github.com>
2025-01-20 00:18:43 +0900

    [level 1] Title: 가장 가까운 같은 글자, Time: 16.18 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/1/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -2,17 +2,12 @@ function solution(s) {
     var answer = [];
     var arr = []
     for(i = 0; i< s.length; i++){
-
         if(!arr.includes(s[i])){
             arr.push(s[i]);
             answer.push(-1)
         }else{
-
-            console.log(arr.length)
-            console.log("  " + arr.lastIndexOf(s[i]))
-
             answer.push(arr.length - arr.lastIndexOf(s[i]))
-                                    arr.push(s[i])
+            arr.push(s[i])
         }
     }
     return answer;
