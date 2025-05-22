e465b09bf237a3f351d93bce57acc8ef37588330
Jisu Kang <90014581+K-jisu@users.noreply.github.com>
2025-05-22 23:14:52 +0900

    [level 1] Title: 콜라 문제, Time: 0.30 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.js
@@ -1,9 +1,14 @@
 function solution(a, b, n) {
+    // 총 얻은 콜라 개수
     let answer = 0;
     
+    // 빈병이 a 보다 크면 loop
     while (n >= a) {
+        // 교환 콜라
         const exchanged = Math.floor(n / a) * b;
+        // 교환한 콜라 누적
         answer += exchanged;
+        // 빈병 = 나머지 + 교환한 콜라
         n = (n % a) + exchanged;
     }
 
