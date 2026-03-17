cfb6ffa1903a84a2e4749c9760036ca197308e34
Jisu Kang <90014581+K-jisu@users.noreply.github.com>
2026-03-17 19:55:26 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,9 +1,1 @@
-function solution(my_string, is_suffix) {
-    const array = [...my_string].reverse().join("")
-    const suffix = [...is_suffix].reverse().join("")
-    if(!my_string.includes(is_suffix)){
-        return 0
-    }else{
-        return array.slice(0, is_suffix.length).includes(suffix) ? 1 : 0
-    }
-}
+const solution = (str, suff) => str.endsWith(suff) ? 1 : 0
